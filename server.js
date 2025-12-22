ba36e8bc5f7101e13807fb67f456dd2bd767aaa1
TechShri027 <shrishtibansal2705@gmail.com>
2025-12-22 15:20:01 +0530

    url

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const authRoutes = require("./routes/authRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 const path = require("path");
 const app=express()
-app.use(cors());             
+app.use(cors({
+  origin: [
+    "https://mernblogpanel.netlify.app/",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));           
 app.use(express.json()); 
 
 
